72779025708e3f597dba3b06462a3029002955ac
“““SYSU-Winter git config --global user.email “Yliuwt1314@gmail.com <liuwt1314@gmail.com>
2017-02-23 13:42:15 +0800

    第一次提交尝试

---
+++ app/src/main/java/com/example/a12136/firsttryapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.a12136.firsttryapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 12136 on 2016/9/22.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linearlayout);
+    }
+}
